309978efa6f4aa4ebd817baab949580aa098ec41
Pablo <itspablo@null.net>
2021-12-06 19:47:58 -0800

    first release

--- dist/win99-plugin.js
+++ dist/win99-plugin.js
@@ -1,1 +1,18 @@
-
+(function() {
+    if (!parent.location == "https://windows99.vercel.app" || !parent.location == "https://windows99dev.vercel.app") {
+        var gg = document.createElement("p");
+        gg.innerText = "The script this app is using (w99-plugin) is not being used on Windows 99, and therefore cannot run.";
+        document.body.appendChild(gg);
+    } else {
+        var head = document.getElementsByTagName("head")[0];
+        var stylelink = document.createElement("link");
+        stylelink.setAttribute("rel", "stylesheet");
+        stylelink.setAttribute("href", parent.document.getElementById("stylelink"));
+        head.appendChild(stylelink)
+        if (document.currentScript.getAttribute("context")) {
+            return;
+        } else {
+            document.body.oncontextmenu = "return false;"; //Turn off context menu for now.
+        }
+    }
+}())
